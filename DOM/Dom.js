9e9466bf3fd39b84f8454ef6aeddd79898e49090
SELVIN R <steelselvin2002@gmail.com>
2025-10-15 16:09:09 +0530

    first commit

---
+++ DOM/Dom.js
@@ -0,0 +1,59 @@
+
+// Dom - Document Object Model
+
+// console.log(window.innerHeight,window.innerWidth);
+
+// window.open("https://www.google.com");
+
+// window.close();
+
+// window.print();
+
+// window.moveTo(500, 100);
+// window.resizeTo(500, 500);
+
+
+
+// Bom - Browser Object Model===============================(Location)==================================================================================
+
+// console.log(screen.width,screen.height);
+
+// console.log(location);
+
+// setInterval(() => {
+
+//       location.href="https://www.google.com";
+
+// },5000);
+
+
+// setInterval(() => {
+
+//     // document.body.style.background="red";
+//     // document.body.style.background=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+//     // // document.body.style.background=`hsl(${Math.random()*360},100%,50%)`;
+//     // document.body.style.background=`#${Math.floor(Math.random()*16777215).toString(16)}`;
+//     // document.title=`Time : ${new Date().toLocaleTimeString()}`;
+
+// },1000);
+
+
+
+// History=================================================================================================================
+
+// console.log(history);
+
+// history.back();
+// history.forward();
+// history.go(-1);
+// history.pushState({name:"sachin"},"","?name=sachin");
+// history.replaceState({name:"sachin"},"","?name=sachin");
+
+
+
+// Navigater=================================================================================================================
+
+
+
+
+  
